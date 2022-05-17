e460ef92ac46131c27915f83ca6387e691c65c09
Focas Kandulo <54008441+Shiru-kun@users.noreply.github.com>
2022-05-17 06:16:31 +0200

    add: movie land learning

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import { useEffect, useState } from 'react';
+import  {useEffect, useState } from "react";
 import './App.css';
+import SearchIcon from './search.svg';
+import Movie from "./Movie";
+const apiKey ="a07b2f38"
+const API_URL = `http://www.omdbapi.com?apiKey=${apiKey}`;
+const App =() =>{
 
+    const [searchTerm, setSearchTerm] = useState("");
+    const [movies, setMovies] = useState([]);
 
-
-const App = () => {
-    const [counter, setCounter] = useState(0);
+    const getMovies = async(title)=>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+     const handlerSearch = (searchTerm) =>{
+        setSearchTerm(searchTerm);
+        getMovies(searchTerm)
+     }
     useEffect(()=>{
-        setCounter(100);
-    },[]);
+        setSearchTerm('batman')
+        getMovies(searchTerm);
+
+  }, []);
 
     return (
-        <div className='App'>
-            <button onClick={()=> setCounter((previous)=>previous-1)}>-</button>
-            <h1>{counter}</h1>
-            <button onClick={()=>setCounter((previous)=>previous+1)}>+</button>
+        <div className="app">
+      <h1>MovieLand</h1>
 
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => handlerSearch(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => getMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <Movie movie={movie} />
+          ))}
         </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
     );
 }
 
